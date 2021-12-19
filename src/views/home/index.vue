b6d72cdda47ea26f7217cfa6eaec8c6c5edd9dfa
zhangting <273664273@qq.com>
2021-12-19 10:51:37 +0800

    下载自动导入插件前提交

---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="active">Home</div>
+</template>
+<style scoped lang='less'>
+// 底层webpack解析路径时要求添加一个波浪线
+// @import '~@/styles/variables.less';
+// 本来每次都需要手动导入的,但是下了一个webpack的插件,可以自动导入
+.active {
+  color: @xtxColor;
+}
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- 顶部的通栏 -->
+  <div>顶部通栏</div>
+  <!-- 顶部导航栏 -->
+  <div>导航栏</div>
+  <!-- 路由填充位 -->
+  <router-view />
+  <!-- 底部的footer -->
+  <div>Footer</div>
+</template>
